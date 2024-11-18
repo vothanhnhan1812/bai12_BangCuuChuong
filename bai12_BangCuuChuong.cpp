6eb64d73b6434519907206dacf7f7669874d5320
MAY 06 <MAY 06@LAB2-16>
2024-11-18 10:28:27 +0700

    Add project files.

---
+++ bai12_BangCuuChuong.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 2; i <= 9; i++) {
+        cout << "Bang cuu chuong " << i << ":" << endl;
+        for (int j = 2; j <= 9; j++) {
+            cout << i << " x " << j << " = " << i * j << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
